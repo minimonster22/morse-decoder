89118d92ad4c328668a41fddf50b878c7fa6ac24
minimonster22 <tamaramonstr@gmail.com>
2023-10-29 22:38:23 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const morseToChar = (morse) => {
+        if (morse === '**********') {
+            return ' ';
+        }
+        // Убираем ведущие нули, затем заменяем 10 на ".", 11 на "-"
+        morse = morse.replace(/^0+/, '').replace(/10/g, '.').replace(/11/g, '-');
+        return MORSE_TABLE[morse];
+    }
+
+    const words = expr.split('**********');
+    const decodedWords = words.map((word) => {
+        const letters = word.match(/.{1,10}/g);
+        const decodedLetters = letters.map(morseToChar);
+        return decodedLetters.join('');
+    });
+
+    return decodedWords.join(' ');
 }
 
 module.exports = {
